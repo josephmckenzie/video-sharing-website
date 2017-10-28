c2d402b87971b1ecea8717724e42e104656c442e
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-10-28 07:24:03 -0400

    challange one completed where we had to remove all but the last period in a file name as well as spaces, worked on getting lambda to run with pipeline from a callout locally

--- video-transcoding-pipeline (pt-1)/lambda/index.js
+++ video-transcoding-pipeline (pt-1)/lambda/index.js
@@ -1,23 +1,15 @@
-/**
- * Created by Peter Sbarski
- * Updated by Mike Chambers
- * Last Updated: 1/02/2017
- *
- * Required Env Vars:
- * ELASTIC_TRANSCODER_REGION
- * ELASTIC_TRANSCODER_PIPELINE_ID
- */
 
-//We will want to add the capability of reading an enviromental variable from a local .env and loading it in to process.env
-//I'll check and see if this is actually required here as it wasnt in here originally but I usually always add it in my code, but I also have nver tried an env inside of a .json file so it may be for that. Ill update the comments to reflect once I find out for sure.
+
+//We will want to add the capability of reading an environmental variable from a local .env and loading it in to process.env
+//I'll check and see if this is actually required here as it wasnt in here originally but I usually always add it in my code, but I also have never tried an env inside of a .json file so it may be for that. Ill update the comments to reflect once I find out for sure.
 require('dotenv').config();
 
 'use strict';
 //We require the aws node module so we can easly interact with Amazon's Api
 var AWS = require('aws-sdk');
-//We first need to start a new instance of Amazon's Elastic Transcoder that will transcode the video in to the correct formats and bitrate for a users given device
+//We first need to start a new instance of Amazon's Elastic Transcoder that will transcode the video in to the correct formats and bit rate for a users given device
 var elasticTranscoder = new AWS.ElasticTranscoder({
- //**** Use an enviromental variable as not to upload any sensitive account credentials or information
+ //**** Use an enviroment variable as not to upload any sensitive account credentials or information
     region: process.env.ELASTIC_TRANSCODER_REGION
 });
 
@@ -30,14 +22,16 @@ exports.handler = function(event, context, callback){
     console.log(key)
     //we check the name of the input file as some may have spaces in them and if they do replaces them with a '+'
     //Using a /g (which is global) will ensure that all spaces are replaced because by default the .replace method only updates the first value needing modified it comes to.
-    var sourceKey = decodeURIComponent(key.replace(/\+/g, ' '));
-
+     var sourceKey = decodeURIComponent(key.replace(/\s+/g, '+'));
     // next we will want to remove the extension from the file name
-    // Here I may have to change this up to account for any periods in the file name unless we account for that before video upload.
-    var outputKey = sourceKey.split('.')[0];
+    //Having changed my regexp expression I now can remove all the periods in a filename except the last one before the extension type
+      var rogue_period_replacer = sourceKey.replace(/\.(?=.*\.)/g,'+');
+
+    // Having replaced all periods except for the very last one we can now remove the one for the extension as well so that we can have an output key which is the file name without an extension or non alphanumeric 
+    var outputKey = rogue_period_replacer.split('.')[0];
 
     var params = {
-     // Once again we want to use an enviromental variable to to ensure our secure account information is kept confidental
+     // Once again we want to use an environmental variable to ensure our secure account information is kept confidential
         PipelineId: process.env.ELASTIC_TRANSCODER_PIPELINE_ID,
      //our outputKey is the file name after all spaces have been  updated to a '+'  and the extension type removed  
      OutputKeyPrefix: outputKey + '/',
@@ -59,7 +53,7 @@ exports.handler = function(event, context, callback){
                 PresetId: '1351620000001-100070' //Web Friendly 720p
             }
         ]};
-// We call upon the elasticTranscoder to create a job passing in the params that include the filename with along with the format and bitrate for it to be transcoded into a more awesome format 
+// We call upon the elasticTranscoder to create a job passing in the params that include the filename with along with the format and bit rate for it to be transcoded into a more awesome format 
     elasticTranscoder.createJob(params, function(error, data){
         if (error){
             callback(error);
